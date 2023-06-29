58b772b7dabe3344d5fbd65ea76fd7fdb964bbf0
yogeshjoga <yogesh.joga@cloudfuze.com>
2023-06-29 11:56:30 +0530

    updated

---
+++ TypeScript/index.js
@@ -0,0 +1,19 @@
+console.log("Hello the World!");
+// lets write the some variables in typescript
+var age = 26;
+// re-assign the values, its not accepted from typeScript
+// age = 'a';
+function add(a, b) {
+    return a + b;
+}
+var c = add2(10, 20);
+console.log(c);
+// let's created a new enum type object in typeScript 
+var persons;
+(function (persons) {
+    persons[persons["Up"] = 1] = "Up";
+    persons[persons["Down"] = 2] = "Down";
+    persons[persons["Side"] = 3] = "Side";
+    persons[persons["Back"] = 4] = "Back";
+})(persons || (persons = {}));
+console.log(persons);
---
+++ TypeScript/index.ts
@@ -0,0 +1,34 @@
+console.log("Hello the World!")
+
+
+// lets write the some variables in typescript
+
+var age: number = 26;
+
+// re-assign the values, its not accepted from typeScript
+// age = 'a';
+
+
+function add(a:number,b:number){
+    return a+b;
+}
+ 
+ var c = add(10,20);
+ console.log(c);
+
+ // let's created a new enum type object in typeScript 
+enum persons{
+    Up   = 1,
+    Down = 2,
+    Side = 3,
+    Back = 4,
+}
+
+console.log(persons);
+
+
+// Lets create a new Array
+
+var num:number[] =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+
+var nums = [];YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY
