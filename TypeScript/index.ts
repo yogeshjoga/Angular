0b6a5c41971d59f9622b4817c097130626a57c15
yogeshjoga <yogesh.joga@cloudfuze.com>
2023-06-29 13:25:33 +0530

    updateed

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -2,21 +2,21 @@ console.log("Hello the World!")
 
 
 // lets write the some variables in typescript
-
 var age: number = 26;
 
 // re-assign the values, its not accepted from typeScript
 // age = 'a';
 
 
-function add(a:number,b:number){
+function add(a:number, b: number):number;
+ function add(a: number,b: number):number{
     return a+b;
 }
  
  var c = add(10,20);
  console.log(c);
 
- // let's created a new enum type object in typeScript 
+// let's created a new enum type object in typeScript 
 enum persons{
     Up   = 1,
     Down = 2,
@@ -31,4 +31,4 @@ console.log(persons);
 
 var num:number[] =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 
-var nums = [];YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY
+var nums = [];
